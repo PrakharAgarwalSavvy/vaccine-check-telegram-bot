c2ce23cd838d3a3f8c85214ffc389960efdb8ffe
PrakharAgarwal <48755902+PrakharAgarwalSavvy@users.noreply.github.com>
2021-06-05 19:56:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
 import pandas as pd
 from copy import deepcopy
+import logging
+import datetime
+import logging.handlers as handlers
+
+logger = logging.getLogger('MAIN_FINAL')
+logger.setLevel(logging.INFO)
+
+logHandler = handlers.TimedRotatingFileHandler('logs/main_logger.log', when='m', interval=1)
+logHandler.setLevel(logging.INFO)
+fileformatter = logging.Formatter('%(asctime)s,%(msecs)d - %(name)s - %(levelname)s - %(message)s')
+logHandler.setFormatter(fileformatter)
+logger.addHandler(logHandler)
 
 def load_mapping():
     df = pd.read_csv("district_mapping.csv")
@@ -31,16 +43,16 @@ def payment_validation(text):
         if (text in valid_payments):
             return True
         else:
-            return False
+            return False  
 
-def set_user_data(context,filter_name,text_input):
-    context.user_data[filter_name]=text_input
+def set_user_data(context,key_name,value):
+    context.user_data[key_name]=value
     
-def set_message_reply(update,context,message,new_filter):
+def set_message_reply(update,context,message,key_name):
     update.message.reply_text(
                 message
             )
-    set_user_data(context,new_filter,1)
+    set_user_data(context,key_name,1)
 
 def send_reply_text(update,message,arg):
     if(arg == 'HTML'):
@@ -70,7 +82,7 @@ def check_empty_df(final_df):
     else:
         return False
         
-def create_error_message_df(message,value,final_df):
+def create_error_message_df(message,value):
     # initialize list of lists
     message = message + value
     data = [[message]]
@@ -79,3 +91,45 @@ def create_error_message_df(message,value,final_df):
     final_df = pd.DataFrame(data, columns = ['message'])
     return final_df
     
+def do_logging(method_name, update_id,message_id,chat_id, e):
+    if e == None:
+        msg = "method name = {} :: update_id = {} :: message_id = {} :: chat_id = {}"
+        logger.debug(msg.format(method_name,update_id,message_id,chat_id))
+    elif(isinstance(e, dict)):
+        msg = "method name = {} :: update_id = {} :: message_id = {} :: chat_id = {} :: UserData :: {}"
+        logger.debug(msg.format(method_name,update_id,message_id,chat_id, e))
+    else:
+        msg = "method name = {} :: update_id = {} :: message_id = {} :: chat_id = {} :: Exception :: {}"
+        logger.error(msg.format(method_name,update_id,message_id,chat_id, e))
+        
+def isSubscribed(userdata):
+    isSubscribe= userdata.get('subscribe')
+    if(isSubscribe == None or isSubscribe != 'yes'):
+        return False
+    else:
+        return True
+        
+def print_filters(update,context):
+    msg="";
+    for key, value in context.user_data.items():
+            if(key == 'dist_name'):
+                msg = msg + '\n<b>Distict Name : </b>' + value 
+            if(key == 'date_range'):
+                if(value == '1'):
+                    msg = msg + '\n<b>Date Range </b>: ' + value + ' (Today)'
+                else:
+                    msg = msg + '\n<b>Date Range </b>: ' + value + ' days from today.'
+            if(key == 'pincode' and value != '000000'):
+                msg = msg + '\n<b>Pincode </b>: ' + value 
+            if(key == 'age_limit'):
+                msg = msg + '\n<b>Age Group </b>: ' + value +'+'
+            if(key == 'payment'):
+                if (value == 'Both'):
+                    msg = msg +  '\n<b>Payment Type </b>: ' + value +' ( Free and paid )'
+                else:
+                    msg = msg +  '\n<b>Payment Type </b>: ' + value
+            if(key == 'vaccine_type'):
+                msg = msg +  '\n<b>Vaccine </b>: ' + value 
+            if(key == 'dose_avail'):
+                msg = msg +  '\n<b>Dose Availability </b>: Dose ' + value  
+    send_reply_text(update,msg,'HTML') 
