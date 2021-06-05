a24834a596b870bbd2d111bcbdf6a66d5ab5beba
PrakharAgarwal <48755902+PrakharAgarwalSavvy@users.noreply.github.com>
2021-06-05 19:54:30 +0530

    Update api_final.py

--- api_final.py
+++ api_final.py
@@ -1,4 +1,3 @@
-import logging
 import datetime
 import json
 import numpy as np
@@ -8,17 +7,9 @@ import config as cfg
 import utils
 from copy import deepcopy
 from typing import Dict
-
-
-logging.basicConfig(filename='logs/api_logger.txt',
-                            filemode='w',
-                            format='%(asctime)s,%(msecs)d - %(name)s - %(levelname)s - %(message)s',
-                            datefmt='%H:%M:%S',
-                            level=logging.DEBUG)
-logger = logging.getLogger("API_FINAL")
     
-def get_availability(answers: Dict[str, str]):
-   logger.debug(f'{answers} -> user_input.')
+def get_availability(answers: Dict[str, str],update):
+   utils.do_logging('get_availability',update.update_id,update.message.message_id,update.message.chat_id, answers)
    DIST_ID = answers.get('dist_id')
    base = datetime.datetime.today()
    date_list = [base + datetime.timedelta(days=x) for x in range(int(answers.get('date_range')))]
@@ -31,6 +22,8 @@ def get_availability(answers: Dict[str, str]):
        if (response.ok) and ('centers' in json.loads(response.text)):
            resp_json = json.loads(response.text)['centers']
            if resp_json is not None: 
+               if final_df is None:
+                  final_df = pd.DataFrame()
                df = pd.DataFrame(resp_json)
                if len(df):
                    df = df.explode("sessions")
@@ -47,26 +40,43 @@ def get_availability(answers: Dict[str, str]):
                               df.vaccine_fees)  
                       df['fee_amount'] = df.vaccine_fees.apply(lambda x: x.get('fee'))
                    except:
-                      df['fee_amount'] = 'Unknown'
-                      logger.debug('No vaccine_fees column in dataframe')
-                   df = df[["date", "available_capacity", "vaccine", "min_age_limit", "pincode", "name", "state_name", "district_name", "block_name", "fee_type","fee_amount","available_capacity_dose1","available_capacity_dose2"]]
-                   if final_df is not None:
+                      df['fee_amount'] = np.where(df.fee_type == 'Free',
+                              dict({"fee": '0'}),
+                              dict({"fee": 'unknown'}))
+                      df['fee_amount'] = df.fee_amount.apply(lambda x: x.get('fee'))
+                      utils.do_logging('get_availability',update.update_id,update.message.message_id,update.message.chat_id, 'No vaccine_fees column in dataframe')
+                   df = df[["date", "available_capacity", "vaccine", "min_age_limit", "pincode", "name", "address","state_name", "district_name", "block_name", "fee_type","fee_amount","available_capacity_dose1","available_capacity_dose2"]]
+                   if not final_df.empty:
                        final_df = pd.concat([final_df, df])
                    else:
-                       final_df = deepcopy(df)                  
-               else:
-                   return utils.create_error_message_df(cfg.NO_CENTER_MESSAGE,'',final_df)
-
-   reset_df = filter(final_df,answers,date_str)
+                       final_df = deepcopy(df)                        
+   if final_df is None:
+      utils.do_logging('get_availability ',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'final_df is none'}))
+      reset_df = utils.create_error_message_df(cfg.TECHNICAL_ERROR_MESSAGE,'')
+   elif(final_df.empty):    
+      utils.do_logging('get_availability ',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'final_df is empty'}))
+      reset_df = utils.create_error_message_df(cfg.NO_CENTER_MESSAGE,'')
+   else:       
+      utils.do_logging('get_availability ',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'final_df send to filter'}))
+      reset_df = filter(final_df,answers,date_str,update)
    return reset_df
 
-def filter(final_df,answers,date_str):
+def filter(final_df,answers,date_str,update):
     message=''
-    if (final_df is not None) and (len(final_df)):
+    if (not final_df.empty) and (len(final_df)):
+        with open('filter_org.csv', 'w') as f:
+            f.write(final_df.to_csv(index=False))
         final_df.drop_duplicates(inplace=True)
         final_df.rename(columns=cfg.RENAME_MAPPING, inplace=True)
         new_df= None
-        logger.debug('date filter start')
+        
+        utils.do_logging('filter',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'availability filter start'}))
+        final_df = utils.filter_capacity(final_df, "Available Capacity", 0)
+        isEmpty = utils.check_empty_df(final_df)
+        if bool(isEmpty):
+            return utils.create_error_message_df(cfg.NO_VACCINE_MESSAGE,'')
+
+        utils.do_logging('filter',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'date filter start'}))
         date_range_str = answers.get('date_range')
         for date_inp in date_str: 
             date_df = utils.filter_column(final_df, "Date", date_inp)
@@ -75,8 +85,9 @@ def filter(final_df,answers,date_str):
         isEmpty = utils.check_empty_df(final_df) 
         if bool(isEmpty):
             message = f'No Vaccination centers are available for booking in next {date_range_str} day/days'
-            return utils.create_error_message_df(message,'',final_df)
-        logger.debug('pincode filter start')
+            return utils.create_error_message_df(message,'')
+        
+        utils.do_logging('filter',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'pincode filter start'}))
         valid_pincodes = list(np.unique(final_df["Pincode"].values))
         pincode_inp_str=answers.get('pincode')
         pincode_inp= int(pincode_inp_str)
@@ -84,30 +95,56 @@ def filter(final_df,answers,date_str):
             final_df = utils.filter_column(final_df, 'Pincode', pincode_inp)
             isEmpty = utils.check_empty_df(final_df)
             if bool(isEmpty):
-                return utils.create_error_message_df(cfg.NO_VACCINE_PINCODE_MESSAGE,pincode_inp_str,final_df)
+                return utils.create_error_message_df(cfg.NO_VACCINE_PINCODE_MESSAGE,pincode_inp_str)
+        elif(pincode_inp == 000000 ):
+            utils.do_logging('filter',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'only district choosen'}))
         else:
-            return utils.create_error_message_df(cfg.NO_VACCINE_PINCODE_MESSAGE,pincode_inp_str,final_df)
-        logger.debug('min age filter start')
+            return utils.create_error_message_df(cfg.NO_VACCINE_PINCODE_MESSAGE,pincode_inp_str)
+        utils.do_logging('filter',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'min age filter start'}))
         age_inp = int(answers.get('age_limit'))
         final_df = utils.filter_column(final_df, "Minimum Age Limit", age_inp)
         isEmpty = utils.check_empty_df(final_df)
         if bool(isEmpty):
-           message = 'No slots open for Age above '+str(age_inp) + ' ' + ' in pincode ' + str(pincode_inp)
-           return utils.create_error_message_df(message,'',final_df)
-        logger.debug('payment filter start')    
+           if(pincode_inp == 000000):
+              message = 'No slots are available for '+str(age_inp) +'+ age group.'
+           else:
+              message = 'No slots are available for '+str(age_inp) + '+ age group in pincode ' + str(pincode_inp)
+           return utils.create_error_message_df(message,'')  
+        
+        utils.do_logging('filter',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'payment filter start'}))        
         pay_inp= answers.get('payment')
         if pay_inp != "Both":
             final_df = utils.filter_column(final_df, "Fees", pay_inp)
             isEmpty = utils.check_empty_df(final_df)
             if bool(isEmpty):
                 message = pay_inp + ' vaccines are not available for '+str(age_inp) + '+ age group' + ' in ' + str(pincode_inp)
-                return utils.create_error_message_df(message,'',final_df)
-        logger.debug('availability filter start') 
-        final_df = utils.filter_capacity(final_df, "Available Capacity", 0)
-        isEmpty = utils.check_empty_df(final_df)
-        if bool(isEmpty):
-            logging.debug(cfg.DATAFRAME_EMPTY_MESSAGE)
-            return utils.create_error_message_df(cfg.NO_VACCINE_MESSAGE,'',final_df)
+                return utils.create_error_message_df(message,'')
+        
+        distinct_vaccine = final_df.Vaccine.unique().tolist()
+        utils.do_logging('filter',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'vaccine type filter start'}))        
+        vaccine_type= answers.get('vaccine_type')
+        if (vaccine_type): 
+            if (vaccine_type in distinct_vaccine):
+                final_df = utils.filter_column(final_df, "Vaccine", vaccine_type)
+                isEmpty = utils.check_empty_df(final_df)
+                if bool(isEmpty):
+                    message = vaccine_type + 'vaccine are not available for chosen filters.'
+                    return utils.create_error_message_df(message,'')
+            else:
+                message = vaccine_type + ' vaccines are not available in any center for chosen filters.'
+                return utils.create_error_message_df(message,'')
+        
+        utils.do_logging('filter',update.update_id,update.message.message_id,update.message.chat_id, dict({"message":'specific dose filter start'}))
+        dose_avail= answers.get('dose_avail')
+        if(dose_avail):
+            if(dose_avail == '1'):
+                final_df = utils.filter_capacity(final_df, "Dose 1 Available Capacity", 0)
+            else:
+                final_df = utils.filter_capacity(final_df, "Dose 2 Available Capacity", 0)
+            isEmpty = utils.check_empty_df(final_df)
+            if bool(isEmpty):
+                return utils.create_error_message_df('Dose ' + dose_avail + ' vaccines are not availabile','')
+                  
 
         reset_df = deepcopy(final_df)
         reset_df.reset_index(inplace=True, drop=True)
@@ -115,5 +152,3 @@ def filter(final_df,answers,date_str):
     else:
         reset_df = pd.DataFrame([[cfg.TECHNICAL_ERROR_MESSAGE]], columns = ['message'])
     return reset_df;
-       
-        
